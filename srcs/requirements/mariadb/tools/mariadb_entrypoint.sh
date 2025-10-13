80b95b7a90ce88b7b42a0025b1473399b60d9a52
gxsilva_user <lssvgabriel@gmail.com>
2025-10-13 18:43:54 -0300

    feat: Mariadb implementation

---
+++ srcs/requirements/mariadb/tools/mariadb_entrypoint.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+set -e
+
+#MACROS FUNCTIONS
+log_info()    { echo "ℹ️ [INFO] $*"; }
+log_file()    { echo "📄 [FILE] $*"; }
+log_success() { echo "✅ [OK] $*"; }
+log_error()   { echo "❌ [ERROR] $*" >&2; }
+log_dir()     { echo "📁 [DIR] $*"; }
+log_warn()    { echo "⚠️ [WARN] $*"; }
+
+#UNSET SECRETS VARIABLE
+unset_secrets()
+{
+    if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
+        unset MYSQL_ROOT_PASSWORD
+    fi
+    if [ -z "$MYSQL_PASSWORD" ]; then
+        unset MYSQL_PASSWORD
+    fi
+}
+
+#DEBUG CONFIGURATION
+if [ "${DEBUG:-}" = "true" ]; then
+    set -x
+    log_info "Debug mode ENABLE"
+else
+    log_info "Debug mode DISABLE"
+fi
+
+#SECRETS MANAGEMENT
+if [ -z "${MYSQL_PASSWORD:-}" ] && [ -f "${MYSQL_SP_PASSWORD:-}" ]; then
+    MYSQL_PASSWORD=$(<"${MYSQL_SP_PASSWORD}")
+else
+    log_error "Failed to initialize MYSQL_PASSWORD. File not found or empty at path: ${MYSQL_SP_PASSWORD}"
+    false
+fi
+
+if [ -z "${MYSQL_ROOT_PASSWORD:-}" ] &&  [ -f "${MYSQL_SP_ROOT_PASSWORD}" ]; then
+    MYSQL_ROOT_PASSWORD=$(<"${MYSQL_SP_ROOT_PASSWORD}")
+else
+    log_error "Failed to initialize MYSQL_ROOT_PASSWORD. File not found or empty at path: ${MYSQL_SP_ROOT_PASSWORD}"
+    false
+fi
+
+#MARIADB INSTALL AND CONFIGURATION
+if [ ! -f "/var/lib/mysql/.initialized" ]; then
+    log_info "Initialize Data Base..."
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql --rpm --skip-test-db
+    
+    log_info "Start temporary instance for configuration..."
+
+    mysqld --user=mysql --datadir=/var/lib/mysql --skip-networking --socket=/tmp/mysql_init.sock &
+    MYSQL_PID="$!"
+
+    until mysqladmin ping --socket=/tmp/mysql_init.sock --silent; do
+        log_info "Database being configured"
+        sleep 1
+    done
+
+    mysql --socket=/tmp/mysql_init.sock -u root << EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '$MYSQL_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+    kill "$MYSQL_PID"
+    wait "$MYSQL_PID"
+    
+    touch /var/lib/mysql/.initialized
+    log_success "Data Base initialization completed"
+else
+    log_success "Data Base already initialize, skipping bootstrap script..."
+fi
+
+unset_secrets
+
+log_success "Starting mariadb with tini as PID 1..."
+
+exec mysqld --user=mysql --datadir=/var/lib/mysql
+
+#---------------(DEPRECATED BY ME XD)----------------------------------
+# if ! MYSQL_ROOT_PASSWORD="$MYSQL_ROOT_PASSWORD" \
+#     MYSQL_PASSWORD="$MYSQL_PASSWORD" \
+#     MYSQL_DATABASE="$MYSQL_DATABASE" \
+#     MYSQL_USER="$MYSQL_USER" \
+#     envsubst '$MYSQL_ROOT_PASSWORD $MYSQL_PASSWORD $MYSQL_DATABASE $MYSQL_USER' \
+#     < /etc/mysql/mariadb.conf.d/wordpress.sql \
+#     | mysql --socket=/tmp/mysql_init.sock -u root
+# then
+#     echo "⚠️ Failed to initialize the database with wordpress.sql"
+#     kill "$MYSQL_PID"
+#     wait "$MYSQL_PID"
+    
+#     # Cleanup sensitive vars if unset or empty
+#     [ -z "$MYSQL_ROOT_PASSWORD" ] && unset MYSQL_ROOT_PASSWORD
+#     [ -z "$MYSQL_SP_PASSWORD" ] && unset MYSQL_SP_PASSWORD
+
+#     exit 1
+# fi
+#------------------------------------------------------------
--- srcs/requirements/nginx/tools/nginx_entypoint.sh
+++ srcs/requirements/nginx/tools/nginx_entypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+#MACROS FUNCTIONS
 log_info()    { echo "ℹ️ [INFO] $*"; }
 log_file()    { echo "📄 [FILE] $*"; }
 log_success() { echo "✅ [OK] $*"; }
@@ -8,10 +9,18 @@ log_error()   { echo "❌ [ERROR] $*" >&2; }
 log_dir()     { echo "📁 [DIR] $*"; }
 log_warn()    { echo "⚠️ [WARN] $*"; }
 
+#DEBUG CONFIGURATION
+if [ "${DEBUG:-}" = "true" ]; then
+    set -x
+    log_info "Debug mode ENABLE"
+else
+    log_info "Debug mode DISABLE"
+fi
+
+#NGINX CONFIGURATION
 if [ -f /etc/nginx/nginx.conf ]; then
     log_info "Configuration Process..."
 
-    #---------------------------------------------------------------
     file "Generating /etc/nginx/nginx.conf from ./config/nginx.conf..."
     envsubst '\$NGINX_URL \$NGINX_USER \$NGINX_ROOT_FOLDER  \$NGINX_HTTPS_PORT \$OPENSSL_PATH \$OPENSSL_PVKEY_PATH \$OPENSSL_CA' \
                 < /etc/nginx/nginx.conf > /tmp/nginx.conf &&  \
@@ -26,5 +35,5 @@ if ! nginx -t; then
     false
 fi
 
-log_success "Starting nginx with tini as PID 1..."
+echo "Starting nginx with tini as PID 1..."
 exec /usr/bin/tini -- "$@"
